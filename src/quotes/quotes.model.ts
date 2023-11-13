41bf321afeb80d527a7379a00e0d9bf4e4303ef3
Ricardo Vieira <franciscorvpinheiro49@gmail.com>
2023-11-13 20:14:14 -0300

    Changes endpoints, handles crashes, implements a better search

--- src/quotes/quotes.model.ts
+++ src/quotes/quotes.model.ts
@@ -2,5 +2,5 @@ import * as mongoose from 'mongoose';
 
 export const QuotesSchema = new mongoose.Schema({
   author: { type: String, required: true },
-  quote: { type: String, required: true }
+  quote: { type: String, required: true}
 });
