5e1eba75a1db3d9495dd0d7f5ce07ba54e63264a
Ricardo Vieira <franciscorvpinheiro49@gmail.com>
2023-11-17 14:59:49 -0300

    Minor changes

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { UsersDto } from './dtos/create-users.dto';
@@ -7,117 +11,111 @@ import { CreateQuoteDto } from 'src/quotes/dtos/create-quote.dto';
 
 @Injectable()
 export class UsersService {
-    constructor(
-        @InjectModel('Users') private readonly usersModel: Model<UsersDto>,
-        @InjectModel('Quotes') private readonly quoteModel: Model<CreateQuoteDto>
-    ) {}
+  constructor(
+    @InjectModel('Users') private readonly usersModel: Model<UsersDto>,
+    @InjectModel('Quotes') private readonly quoteModel: Model<CreateQuoteDto>,
+  ) {}
 
-    async hashPassword(password: string){
+  async hashPassword(password: string) {
+    const salt = process.env.SALT;
 
-        const salt = process.env.SALT
+    const passwordToBeHashed = password;
 
-        const passwordToBeHashed = password
+    const hash = await bcrypt.hash(passwordToBeHashed, salt);
 
-        const hash = await bcrypt.hash(passwordToBeHashed, salt);
+    return hash;
+  }
 
-        return hash
+  async checkIfUserExists(username: string) {
+    const findUser = await this.usersModel
+      .findOne({
+        username: { $regex: new RegExp(username, 'i') },
+      })
+      .exec();
+
+    return findUser;
+  }
+
+  async listAllUsers() {
+    const allQuotes = await this.usersModel.find().exec();
+    return allQuotes;
+  }
+
+  async registerUser(createUserDto: UsersDto) {
+    const userSearch = await this.checkIfUserExists(createUserDto.username);
+
+    if (userSearch === null) {
+      const hashedPassword = await this.hashPassword(createUserDto.password);
+
+      createUserDto.password = hashedPassword;
+
+      const user = await new this.usersModel(createUserDto).save();
+
+      return {
+        id: user.id,
+        message: `User ${user.username} created successfully.`,
+      };
     }
 
-    async checkIfUserExists(username:string){
-        const findUser = await this.usersModel
-                    .findOne({ 
-                        username: { $regex: new RegExp(username, 'i') } 
-                    })
-                    .exec()
+    if (userSearch.username === createUserDto.username) {
+      throw new ConflictException('This user already exists.');
+    }
+  }
 
-        return findUser  
+  async deleteUser(id: string) {
+    const user = await this.usersModel.deleteOne({ _id: id }).exec();
+
+    if (user.deletedCount === 0) {
+      throw new NotFoundException(`Quote id ${id} not found.`);
     }
 
-    async listAllUsers() {
-        const allQuotes = await this.usersModel.find().exec()                
-        return allQuotes   
+    return user.acknowledged;
+  }
+
+  async likeQuote(username: string, quoteId: string) {
+    const user = await this.usersModel.findOne({ username }).exec();
+
+    if (!user) {
+      throw new NotFoundException('User not found.');
     }
 
-    async registerUser(createUserDto: UsersDto){
+    if (!user.likedQuotes.includes(quoteId)) {
+      user.likedQuotes.push(quoteId);
 
-        const userSearch = await this.checkIfUserExists(createUserDto.username)
+      return user.save();
+    }
+    return user;
+  }
 
-        if (userSearch === null) {
+  async removeLikedQuote(username: string, quoteId: string) {
+    return await this.usersModel
+      .findOneAndUpdate(
+        { username: username },
+        { $pull: { likedQuotes: quoteId } },
+        { new: true },
+      )
+      .exec();
+  }
 
-            const hashedPassword = await this.hashPassword(createUserDto.password)
+  async listLikedQuotes(username: string) {
+    const user = await this.usersModel.findOne({ username }).exec();
 
-            createUserDto.password = hashedPassword
-
-            const user = await new this.usersModel(createUserDto).save();
-
-            return { 
-                id: user.id, 
-                message: `User ${user.username} created successfully.`
-            }
-        }
-
-        if (userSearch.username === createUserDto.username) {
-            throw new  ConflictException('This user already exists.')
-        }
+    if (!user) {
+      return [];
     }
 
-    async deleteUser(id: string){
-        const user = await this.usersModel.deleteOne({ _id: id }).exec();
+    const quotesArray = [];
 
-        if (user.deletedCount === 0){
-            throw new NotFoundException(`Quote id ${id} not found.`)
-        }
+    for (const quoteId of user.likedQuotes) {
+      const quote = await this.quoteModel.findById(quoteId).exec();
 
-        return user.acknowledged
-    }
-
-    async likeQuote(username: string, quoteId: string){
-        const user = await this.usersModel.findOne({ username }).exec();
-
-        if (!user) {
-          throw new NotFoundException('User not found.');
-        }
-    
-        if (!user.likedQuotes.includes(quoteId)) {
-
-          user.likedQuotes.push(quoteId)
-
-          return user.save()
-        }
-        return user;
+      if (quote) {
+        quotesArray.push({
+          author: quote.author,
+          quote: quote.quote,
+        });
       }
-
-    async removeLikedQuote(username: string, quoteId: string){
-        return await this.usersModel.findOneAndUpdate(
-            { username: username },
-            { $pull: { likedQuotes: quoteId } },
-            { new: true },
-          ).exec();
     }
-
-    async listLikedQuotes(username: string) {
-        
-        const user = await this.usersModel.findOne({ username }).exec();
-
-        if (!user) {
-            return [];
-          }
-
-        const quotesArray = []
-
-        for (const quoteId of user.likedQuotes) {
-            const quote = await this.quoteModel.findById(quoteId).exec();
-            
-            if (quote) {
-                quotesArray.push(
-                    { 
-                       author: quote.author,
-                       quote: quote.quote
-                    }
-                );
-            }
-        }
-        return quotesArray
-    }     
-
+    return quotesArray;
+  }
 }
